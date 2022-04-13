84ed17d3f2e3fd34021acd10292c7287830bb30d
selpeca <ser.per.eli@gmail.com>
2022-04-12 23:46:52 -0500

    Creación de screens

---
+++ src/screens/RankingScreen.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function RankingScreen() {
+  return (
+    <View>
+      <Text>RankingScreen</Text>
+    </View>
+  )
+}
---
+++ src/screens/RestaurantScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export function RestaurantScreen (){
+    return (
+        <View>
+            <Text>Estamos en la screen Restaurants</Text>
+        </View>
+    );
+}
