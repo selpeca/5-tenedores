11a34e8268db19486af412498f99d5ec31f96135
selpeca <ser.per.eli@gmail.com>
2022-04-14 21:00:41 -0500

    accounts views

--- src/screens/Accounts/AccountScreen.js
+++ src/screens/Accounts/AccountScreen.js
@@ -1,10 +1,16 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth"
+import {UserGuestScreen} from "./UserGuestScreen";
+import {UserLoggedScreen} from "./UserLoggedScreen";
 
 export function AccountScreen () {
-    return (
-        <View>
-            <Text>Estamos en la screen AccountScreen</Text>
-        </View>
-    );
+    const [hasLogged, setHasLogged] = useState(null);
+
+    useEffect( () => {
+        const auth = getAuth();
+        onAuthStateChanged(auth, (user) => {
+            setHasLogged(user ? true : false);
+        })
+    },[]);
+    return hasLogged ? <UserLoggedScreen/>:<UserGuestScreen/>;
 }
